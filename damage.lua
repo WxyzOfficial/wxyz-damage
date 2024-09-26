d0668803d2f6ba5f8695eb6ec537b4783dbe836e
WxyzOfficial <99957990+WxyzOfficial@users.noreply.github.com>
2024-09-26 08:45:23 +0800

    Add files via upload

---
+++ damage.lua
@@ -0,0 +1,3 @@
+CreateThread(function()
+    SetWeaponDamageModifier(`WEAPON_UNARMED`, 0.1)
+  end)
---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Wxyz'
+description 'Melee Damage For Qbcore'
+version '1.0.0'
+
+client_script 'damage.lua'
